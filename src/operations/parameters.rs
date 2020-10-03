c93f6f5902309350f4042c2a4259160e9a57ff89
SCingolani <31498644+SCingolani@users.noreply.github.com>
2020-10-03 14:42:11 +0000

    Add serialized descriptions

--- src/operations/parameters.rs
+++ src/operations/parameters.rs
@@ -1,32 +1,35 @@
 
+use serde::Serialize;
+
 use pid::Pid;
 
 
 /// Union type to store the description of some operation; this way we can easily
 /// serialize/dynamically create operations.
 // TODO Is it possible to create a macro that defines this Union?
-pub union OperationDescription {
+#[derive(Serialize)]
+pub enum OperationDescription {
     // OperationName: OperationParams
-    identity: (),
-    pid: PIDOperation,
-    dampenedOscillator: CriticallyDampenerOperation,
-    dlip: ClipOperation,
-    supersample: SupersampleOperation,
-    average: AverageOperation,
+    Identity,
+    PID(PIDOperation),
+    DampenedOscillator(CriticallyDampenerOperation),
+    Clip(ClipOperation),
+    Supersample(SupersampleOperation),
+    Average(AverageOperation),
 }
 
 /// An operation which just reproduces the input iterator
 pub struct IdentityOperation;
 
 /// An operation that implements a PID control
-#[derive(Clone, Copy)]
+#[derive(Serialize, Clone, Copy)]
 pub struct PIDOperation {
     pub pid: Pid<f64>,
     pub offset: u32,
 }
 
 /// An operation which uses a critcially dampened oscillator to reach a target value
-#[derive(Clone, Copy)]
+#[derive(Serialize, Clone, Copy)]
 pub struct CriticallyDampenerOperation {
     pub m: f64,
     pub k: f64,
@@ -35,20 +38,20 @@ pub struct CriticallyDampenerOperation {
 }
 
 /// An operation that clips all values
-#[derive(Clone, Copy)]
+#[derive(Serialize, Clone, Copy)]
 pub struct ClipOperation {
     pub min: f64,
     pub max: f64,
 }
 
 /// An operation that supersamples its input
-#[derive(Clone, Copy)]
+#[derive(Serialize, Clone, Copy)]
 pub struct SupersampleOperation {
     pub n: usize,
 }
 
 /// An operation that supersamples its input
-#[derive(Clone, Copy)]
+#[derive(Serialize, Clone, Copy)]
 pub struct AverageOperation {
     pub n: usize,
 }
