2e584a49dae61e432be452afa7a2c5da4e3bef62
SCingolani <31498644+SCingolani@users.noreply.github.com>
2021-02-09 23:04:54 +0100

    Only output PWM if enough change; only start fan at full speed
    Changes below 1% in output power are ignored. If going from zero power to anything else, first fully power for 500 ms.
    This means that pushing to the output will block; probably need to change this in the future.

--- src/outputs.rs
+++ src/outputs.rs
@@ -6,6 +6,8 @@ use ::std::{thread, time};
 
 use rppal::pwm;
 
+use std::time::Instant;
+
 #[derive(Serialize, Deserialize)]
 pub enum Output {
     PWM,
@@ -20,20 +22,23 @@ pub fn sample_forever<I>(mut source: Box<Iterator<Item = f64>>, mut output: I, r
 where
     I: Pushable,
 {
+    let mut last: f64 = 0.0;
     loop {
-        let next;
-        match source.next() {
-            Some(val) => next = val,
+        let next: f64 = match source.next() {
+            Some(val) => val,
             None => break,
         };
-        let x = next;
-        output.push(x);
+        if (last*100.).round() as u64 != (next*100.).round() as u64 {
+            output.push(next);
+        }
+        last = next;
         thread::sleep(time::Duration::from_millis(rate));
     }
 }
 
 pub struct PWM {
     pin: pwm::Pwm,
+    last_zero: bool,
 }
 
 impl PWM {
@@ -45,12 +50,22 @@ impl PWM {
             pwm::Polarity::Inverse,
             true,
         )?;
-        Ok(PWM { pin: pwm })
+        Ok(PWM { pin: pwm, last_zero: false})
     }
 }
 impl Pushable for PWM {
     fn push(&mut self, val: f64) {
+        const START_POWER: f64 = 100.0;
         debug!("PWM output set to {:2.4}", val / 100_f64);
+        if val < 10.0 { // val is zero?
+            self.last_zero = true;
+        } else if self.last_zero {
+            self.pin.set_duty_cycle(START_POWER / 100_f64).unwrap();
+            thread::sleep(time::Duration::from_millis(500));
+            self.last_zero = false;
+        } else {
+            self.last_zero = false;
+        }
         self.pin.set_duty_cycle(val / 100_f64).unwrap();
     }
 }
