675bbb7f400b1a4847dd530bb0851acffed4b19e
SCingolani <31498644+SCingolani@users.noreply.github.com>
2021-02-09 23:08:22 +0100

    Add small binary utility to print out the current output power

---
+++ src/bin/fan-get-out.rs
@@ -0,0 +1,18 @@
+use std::os::unix::net::UnixStream;
+use std::io::{BufRead,BufReader};
+
+
+fn main() {
+    let stream = BufReader::new(UnixStream::connect("/tmp/fand.socket").unwrap());
+    for line in stream.lines() {
+        let line = line.unwrap();
+        let mut parts = line.split(":");
+        let id = parts.next().unwrap().parse::<usize>().unwrap();
+        let operation_name = parts.next().unwrap().trim();
+        if id == 7 && operation_name == ">" {
+            let the_rest = parts.next().unwrap().parse::<f64>().unwrap();
+            println!("{:2.0}", the_rest);
+            break;
+        }
+    }
+}
